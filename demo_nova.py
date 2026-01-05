14fba5309c6157256aca868cd2b0cd904ea68489
sukhbir143 <sukhbir.deswal@singleinterface.com>
2026-01-05 16:20:16 +0530

    added test file and requirements

--- demo_nova.py
+++ demo_nova.py
@@ -85,27 +85,6 @@ with (sync_playwright() as p):
     mapranktracker = page.wait_for_selector("//span[normalize-space()='Map Rank Tracker']")
     mapranktracker.click()
 
-    # new scan
-    Newscan = page.wait_for_selector("//span[normalize-space()='New Scan']")
-    Newscan.click()
-
-    # Search box
-    searchbox = page.wait_for_selector("//input[contains(@placeholder,'Search by Name, Id, Address, Pincode...')]")
-    searchbox.type("hdfc")
-
-
-    # select brand
-    Selbrand = page.wait_for_selector("//p[normalize-space()='1st Floor, Balurghat, West Bengal']")
-    Selbrand.click()
-
-    # select keyword
-    AIkeywords = page.wait_for_selector("//span[normalize-space()='Cafe service']")
-    AIkeywords.click()
-
-    # Scan Now
-    Scannow = page.wait_for_selector("//span[normalize-space()='Scan Now']")
-    Scannow.click()
-
     # dashboard again
     dashborad = page.wait_for_selector("//a[@href='/dashboard-competition'][normalize-space()='Dashboard']")
     dashborad.click()
